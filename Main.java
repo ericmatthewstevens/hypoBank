1f011abc39ca43b8466d8e2a686c9af9c8a4a3fa
SchnitzelB <es971120@icloud.com>
2021-03-06 19:18:26 -0600

    Created getters for Main.java.

--- Main.java
+++ Main.java
@@ -1,7 +1,28 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main extends acctCalc {
+
+  acctCalc(double accountBalance, double adjustmentAmount) {
+    this.accountBalance = accountBalance;
+    this.adjustmentAmount = adjustmentAmount;
+  }
+
+  public double getAccountBalance(double accountBalance) {
+    return accountBalance;
+  }
+
+  public double getAdjustmentAmount(double adjustmentAmount) {
+    return adjustmentAmount;
+  }
   
   public static void main(String[] args) {
     
+    Scanner scanner = new Scanner(System.in);
+
+      
+    
+    scanner.close();
+
   }
 
 }
--- acctCalc.java
+++ acctCalc.java
@@ -2,9 +2,9 @@ import javax.swing.JOptionPane;
 
 public class acctCalc {
 
-  double accountBalance, adjustmentAmount;
+  public double accountBalance, adjustmentAmount;
 
-  acctCalc(double accountBalance, double adjustmentAmount) {
+  acctCalc(double adjustmentAmount, double accountBalance) {
     this.accountBalance = accountBalance;
     this.adjustmentAmount = adjustmentAmount;
   }
@@ -12,13 +12,21 @@ public class acctCalc {
   public double deposit(double adjustmentAmount, double accountBalance) {
 
     if (adjustmentAmount < 0) {
+
       JOptionPane.showMessageDialog(null, "Please enter a valid USD($) value.");
+
     } else if (adjustmentAmount > 0) {
-      accountBalance += adjustmentAmount;
+
+      accountBalance = accountBalance + adjustmentAmount;
+
       JOptionPane.showMessageDialog(null, "The following amount has been added to your balance." + "\n"
        + "$" + adjustmentAmount + "\n" 
        + "Please allow 1-2 business days for the transaction to complete.");
+       
+       return accountBalance;
+
     }
+
     return accountBalance;
 
   }
@@ -28,16 +36,24 @@ public class acctCalc {
     char decisionChar = ' ';
 
     if (adjustmentAmount < 0) {
+
       JOptionPane.showMessageDialog(null, "Please enter a valid USD($) value.");
+
     } else if (adjustmentAmount > accountBalance) {
+
       JOptionPane.showInputDialog(null, "Warning!" + "\n"
       + "You are about to withdrawl an amount that exceeds your available balance by " + (accountBalance + adjustmentAmount) + "\n"
       + "If you agree to this transaction, your account will be overdrawn and locked, until the overdrawn balance is debited." + "\n"
       + "Do you wish to continue with this transaction? Y/N");
+
       if (decisionChar == 'Y') {
-        accountBalance-=adjustmentAmount;
+
+        accountBalance = accountBalance - adjustmentAmount;
+
       } else if (decisionChar == 'N') {
+
         return accountBalance;
+
       }
     }
 
