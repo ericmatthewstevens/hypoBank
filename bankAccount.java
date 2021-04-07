03186f5886b27033929ae9a5d2a03ac0415f00bc
SchnitzelB <es971120@icloud.com>
2021-04-07 11:01:49 -0500

    Delete two files and add bankAccount super class

---
+++ bankAccount.java
@@ -0,0 +1,33 @@
+public class bankAccount {
+  
+  /** Instance Variables
+   *    PRIVATE
+   *      (int) Account Number
+   *      (int) Routing Number
+   * 
+   *      (String) Client Name
+   *      (String) Date of Birth
+   *      
+   *      (float) Sum of all Account Balances 
+   */
+
+   /** public method, no return, utilizes switch cases
+    *   in order to call different methods
+    *    "Account Access Portal"
+    *     case 1: call checking account;
+    *               extended from checkingAccount sub class;
+    *     case 2: call savings account;
+    *               extended from savingsAccount sub class;
+    *     case 3: call client information;
+    *               within super class
+    *     case 4: exit program;             
+    * 
+    */
+
+    /** public method, no return type
+     *    use terminal to display the client's information
+     *      Account Number, Routing Number, Client Name, Date of Birth
+     * 
+     */
+
+}
